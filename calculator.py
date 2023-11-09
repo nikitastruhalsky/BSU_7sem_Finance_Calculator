e538f6a7e6992f1fc6383e9859633124e2ee6b7f
NikitaStruhalski <strugalsky03@gmail.com>
2023-11-09 17:08:30 +0300

    third step is done

--- calculator.py
+++ calculator.py
@@ -4,32 +4,72 @@ import decimal
 from decimal import Decimal
 
 
-value1, value2 = "", ""
-with st.form("Calculator"):
-    num1 = st.text_area(label='First number', placeholder='Enter first number', value=value1)
-    option = st.selectbox(label='Operation', options=['+', '-', '*', '/'])
-    num2 = st.text_area(label='Second number', placeholder='Enter second number', value=value2)
+def operation(n1: Decimal, n2: Decimal, op):
+    if op == '+':
+        return n1 + n2
+    if op == '-':
+        return n1 - n2
+    if op == '*':
+        return n1 * n2
+    if op == '/':
+        return n1 / n2
+    return 0
+
+
+def has_zero_division(numbers: list, operator_options: list) -> bool:
+    check_list = [True for num, option in zip(numbers, ["filler"] + operator_options) if num == 0 and option == "/"]
+    return len(check_list) > 0
+
+
+def more_priority(op1: str, op2: str) -> bool:
+    if op1 == '+' or op1 == "-":
+        return True
+    else:
+        return False
+
+
+def format_num(num: Decimal) -> str:
+    num = '{:,}'.format(num).replace(',', ' ')
+    num = num.rstrip("0")
+    if num[-1] == '.':
+        num = num[:-1]
+    return num
+
+
+DEFAULT_VALUE = 0
+with st.form("Finance Calculator"):
+    nums = []
+    options = []
+    for i in range(3):
+        nums.append(st.text_area(label=f'Number {i + 1}', placeholder=f'Number {i + 1}', value=DEFAULT_VALUE))
+        options.append(st.selectbox(label=f'Operation {i + 1}', options=["+", "-", "*", "/"]))
+    i += 1
+    nums.append(st.text_area(label=f'Number {i + 1}', placeholder=f'Number {i + 1}', value=DEFAULT_VALUE))
+    round_options = {"Math": decimal.ROUND_HALF_UP,
+                     "Accountant": decimal.ROUND_HALF_EVEN,
+                     "Floor": decimal.ROUND_HALF_DOWN}
+    selected_round_option = round_options[st.selectbox(label="Choose rounding type", options=round_options.keys())]
     submitted = st.form_submit_button("Calculate")
     if submitted:
         try:
-            num1 = Decimal(num1.replace(',', '.').replace(' ', ''))
-            num2 = Decimal(num2.replace(',', '.').replace(' ', ''))
-            six_digits = Decimal('0.000001')
-            if option == '+':
-                res = (num1 + num2).quantize(six_digits, rounding=decimal.ROUND_HALF_UP)
-            if option == '-':
-                res = (num1 - num2).quantize(six_digits, rounding=decimal.ROUND_HALF_UP)
-            if option == '*':
-                res = (num1 * num2).quantize(six_digits, rounding=decimal.ROUND_HALF_UP)
-            if option == '/':
-                if num2 == 0:
-                    st.write('Mistake! Dividing by zero!')
+            nums = [Decimal(x.replace(',', '.').replace(' ', '')) for x in nums]
+            if has_zero_division(nums, options):
+                st.write("Mistake! Division by zero!")
+            else:
+                ten_digits = Decimal('0.0000000001')
+                ans = operation(nums[1], nums[2], options[1]).quantize(ten_digits, rounding=decimal.ROUND_HALF_UP)
+                if more_priority(options[0], options[2]):
+                    ans = operation(ans, nums[3], options[2]).quantize(ten_digits, rounding=decimal.ROUND_HALF_UP)
+                    ans = operation(nums[0], ans, options[0]).quantize(ten_digits, rounding=decimal.ROUND_HALF_UP)
                 else:
-                    res = (num1 / num2).quantize(six_digits, rounding=decimal.ROUND_HALF_UP)
-            res = '{:,}'.format(res).replace(',', ' ')
-            res = res.rstrip("0")
-            if res[-1] == '.':
-                res = res[:-1]
-            st.write(f'Result: {res}'.replace(',', '.'))
+                    ans = operation(nums[0], ans, options[0]).quantize(ten_digits, rounding=decimal.ROUND_HALF_UP)
+                    ans = operation(ans, nums[3], options[2]).quantize(ten_digits, rounding=decimal.ROUND_HALF_UP)
+                six_digits = Decimal('0.000001')
+                integer_num = Decimal('1')
+                full_res = ans.quantize(six_digits, rounding=decimal.ROUND_HALF_UP)
+                full_res = format_num(full_res)
+                rounded = ans.quantize(integer_num, rounding=selected_round_option)
+                st.write(f'Answer: {full_res}'.replace(',', '.'))
+                st.write(f'Rounded Answer: {rounded}'.replace(',', '.'))
         except:
             st.write('Invalid values')
